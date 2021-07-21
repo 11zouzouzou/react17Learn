56286aa3d5a0c84dac117325e458da59caf1a6e6
邹静磊 <zoujl@shinemo.com>
2021-07-21 10:38:01 +0800

    update react UI

--- src/main.tsx
+++ src/main.tsx
@@ -4,10 +4,18 @@ import "./index.css";
 import App from "./App";
 import HelloWorold from "./base/HelloWorld";
 
-ReactDOM.render(
-	<React.StrictMode>
-    {/* <App /> */}
-    <HelloWorold />
-	</React.StrictMode>,
-	document.getElementById("root")
-);
+function tick() {
+	ReactDOM.render(
+		<React.StrictMode>
+			{/* <App /> */}
+			<HelloWorold />
+		</React.StrictMode>,
+		document.getElementById("root")
+	);
+	render();
+}
+function render() {
+	//60帧更新
+	requestAnimationFrame(tick);
+}
+render();
